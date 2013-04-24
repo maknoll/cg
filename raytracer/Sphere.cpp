d80c3311d2649fd8dd66b31523fdabfd760e16f2
Martin Knoll <martinknoll@me.com>
2013-04-24 07:42:08 +0200

    refract0r

--- raytracer/Sphere.cpp
+++ raytracer/Sphere.cpp
@@ -10,53 +10,42 @@ Sphere::closestIntersectionModel(const Ray &ray, real maxLambda) const
 {
   //Programming TASK 1: implement this method
   
-  real r = 1;
-  
   real a = ray.direction().dot(ray.direction());
   real b = 2 * ray.direction().dot(ray.origin());
-  real c = ray.origin().dot(ray.origin()) - ( r * r);
-  
-  //Find discriminant
+  real c = ray.origin().dot(ray.origin()) - 1;
+
   real disc = b * b - 4 * a * c;
   
-  // if discriminant is negative there are no real roots, so return
-  // false as ray misses sphere
   if (disc < 0)
     return false;
   
-  // compute q as described above
-  real distSqrt = sqrtf(disc);
+  real dist_sqrt = sqrtf(disc);
   real q;
   if (b < 0)
-    q = (-b - distSqrt)/2.0;
+    q = (-b - dist_sqrt)/2.0;
   else
-    q = (-b + distSqrt)/2.0;
+    q = (-b + dist_sqrt)/2.0;
   
-  // compute t0 and t1
   real t0 = q / a;
   real t1 = c / q;
   
-  // make sure t0 is smaller than t1
   if (t0 > t1)
   {
-    // if t0 is bigger than t1 swap them around
     real temp = t0;
     t0 = t1;
     t1 = temp;
   }
   
-  // if t1 is less than zero, the object is in the ray's negative direction
-  // and consequently the ray misses the sphere
   if (t1 < 0)
     return false;
   
   real lambda;
-  // if t0 is less than zero, the intersection point is at t1
+  
   if (t0 < 0)
   {
     lambda = t1;
   }
-  // else the intersection point is at t0
+
   else
   {
     lambda = t0;
